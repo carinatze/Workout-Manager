d6b4ec17c6604eae2f562892eeff1e3bf19bdf58
carinatz <s6y2b@students.cs.ubc.ca>
2020-11-02 22:06:52 -0800

    made labels and buttons and input text area

--- src/main/ui/GUI.java
+++ src/main/ui/GUI.java
@@ -1,38 +1,57 @@
 package ui;
 
+import com.sun.corba.se.spi.orbutil.threadpool.Work;
+import model.Workout;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+// citation: https://www.youtube.com/watch?v=iE8tZ0hn2Ws
 // gui class for workout manager
 public class GUI implements ActionListener {
 
     private JFrame frame;
-    private JLabel label;
+    private JLabel workoutNameLabel;
+    private JLabel workoutAddedLabel;
     private JPanel panel;
     private JButton addWorkoutButton;
     private JButton saveCollectionButton;
     private JButton loadCollectionButton;
+    private JTextField workoutNameText;
 
     // EFFECTS: gui constructor for workout manager
     public GUI() {
-        frame = new JFrame();
+        panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0, 1));
 
         addWorkoutButton = new JButton("add workout");
         saveCollectionButton = new JButton("save collection");
         loadCollectionButton = new JButton("load collection");
 
-        addWorkoutButton.addActionListener(this);
-//        label = new JLabel();
-
-        panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        panel.setLayout(new GridLayout(0, 1));
         panel.add(addWorkoutButton);
+
+        workoutNameLabel = new JLabel("Enter new workout name");
+        panel.add(workoutNameLabel);
+
+        workoutNameText = new JTextField(20);
+        workoutNameText.setBounds(100, 20, 165, 25);
+        panel.add(workoutNameText);
+
+        addWorkoutButton.addActionListener(this);
+
+        workoutAddedLabel = new JLabel("");
+        panel.add(workoutAddedLabel);
+
+
         panel.add(loadCollectionButton);
         panel.add(saveCollectionButton);
 
+        loadCollectionButton.addActionListener(this);
+
+        frame = new JFrame();
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Workout Manager");
@@ -46,6 +65,9 @@ public class GUI implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        String workoutName = workoutNameText.getText();
+        Workout workout = new Workout(workoutName);
+        workoutAddedLabel.setText(workoutName + " was added to your collection!");
 
     }
 }
