5028c370f49bfdb19ff4a43e7ffb1ca16fb66d76
carinatz <s6y2b@students.cs.ubc.ca>
2020-11-07 21:20:47 -0800

    fixed methods to checkstyle

--- src/main/ui/WorkoutManagerGUI.java
+++ src/main/ui/WorkoutManagerGUI.java
@@ -38,13 +38,13 @@ public class WorkoutManagerGUI extends JFrame implements ActionListener {
     private JTextField exerciseNameText;
     private JTextField exerciseRepsText;
 
+    private JFrame frame;
+
     private JList<String> workoutJList;
     private DefaultListModel<String> workoutModel;
 
     private JList<String> exerciseJList;
     private DefaultListModel<String> exerciseModel;
-
-    private JFrame frame;
 
     private static final String JSON_STORE = "./data/workout.json";
     private JsonWriter jsonWriter;
@@ -56,9 +56,6 @@ public class WorkoutManagerGUI extends JFrame implements ActionListener {
     // EFFECTS: gui constructor for workout manager
     public WorkoutManagerGUI() {
         frame = new JFrame("Workout Manager");
-        workoutPanel = new JPanel();
-        exercisePanel = new JPanel();
-        splitPane = new JSplitPane();
         workoutModel = new DefaultListModel<>();
         workoutJList = new JList<>(workoutModel);
         exerciseModel = new DefaultListModel<>();
@@ -67,61 +64,16 @@ public class WorkoutManagerGUI extends JFrame implements ActionListener {
         workoutJList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         add(new JScrollPane(workoutJList));
 
-        splitPane.setLeftComponent(workoutPanel);
-        workoutPanel.add(new JLabel("Workouts"));
-        workoutPanel.add(workoutJList);
-
-        splitPane.setRightComponent(exercisePanel);
-        exercisePanel.add(new JLabel("Exercises"));
-        exercisePanel.add(exerciseJList);
-
         jsonWriter = new JsonWriter(JSON_STORE);
         jsonReader = new JsonReader(JSON_STORE);
-
-        workoutNameLabel = new JLabel("workout:");
-        workoutNameText = new JTextField(8);
-        exerciseNameLabel = new JLabel("exercise:");
-        exerciseNameText = new JTextField(8);
-        exerciseRepsLabel = new JLabel("reps:");
-        exerciseRepsText = new JTextField(4);
-
-        addWorkoutButton = new JButton("add workout");
-        addExerciseButton = new JButton("add exercise");
-        saveButton = new JButton("save");
-        loadButton = new JButton("load");
 
         textPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         gridPanel = new JPanel(new GridLayout(2, 1));
 
-        textPanel.add(workoutNameLabel);
-        textPanel.add(workoutNameText);
-        textPanel.add(exerciseNameLabel);
-        textPanel.add(exerciseNameText);
-        textPanel.add(exerciseRepsLabel);
-        textPanel.add(exerciseRepsText);
-
-        buttonPanel.add(addWorkoutButton);
-        buttonPanel.add(saveButton);
-        buttonPanel.add(loadButton);
-        buttonPanel.add(addExerciseButton);
-
-        gridPanel.add(textPanel);
-        gridPanel.add(buttonPanel);
-
-        add(gridPanel, BorderLayout.SOUTH);
-        add(splitPane, BorderLayout.CENTER);
-
-        workoutJList.getSelectionModel().addListSelectionListener(e -> {
-            workoutJList.getSelectedValue();
-            Workout w = collection.getWorkout(workoutJList.getSelectedValue());
-            exerciseModel.removeAllElements();
-            for (int i = 0; i < w.getExercises().size(); i++) {
-                String exerciseString = w.getExercises().get(i).getReps() + " "
-                        + w.getExercises().get(i).getExerciseName();
-                exerciseModel.addElement(exerciseString);
-            }
-        });
+        setUpButtons();
+        setUpLayout();
+        selectionActionListener();
 
         addWorkoutButton.setActionCommand("add workout");
         addExerciseButton.setActionCommand("add exercise");
@@ -142,45 +94,129 @@ public class WorkoutManagerGUI extends JFrame implements ActionListener {
         app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
+    public void selectionActionListener() {
+        workoutJList.getSelectionModel().addListSelectionListener(e -> {
+            workoutJList.getSelectedValue();
+            Workout w = collection.getWorkout(workoutJList.getSelectedValue());
+            exerciseModel.removeAllElements();
+            for (int i = 0; i < w.getExercises().size(); i++) {
+                String exerciseString = w.getExercises().get(i).getReps() + " "
+                        + w.getExercises().get(i).getExerciseName();
+                exerciseModel.addElement(exerciseString);
+            }
+        });
+    }
+
+    // EFFECTS: sets up buttons, labels and text fields
+    public void setUpButtons() {
+        workoutNameLabel = new JLabel("workout:");
+        workoutNameText = new JTextField(8);
+        exerciseNameLabel = new JLabel("exercise:");
+        exerciseNameText = new JTextField(8);
+        exerciseRepsLabel = new JLabel("reps:");
+        exerciseRepsText = new JTextField(4);
+
+        addWorkoutButton = new JButton("add workout");
+        addExerciseButton = new JButton("add exercise");
+        saveButton = new JButton("save");
+        loadButton = new JButton("load");
+    }
+
+    // EFFECTS: sets up layout and panels to GUI
+    public void setUpLayout() {
+        workoutPanel = new JPanel();
+        exercisePanel = new JPanel();
+        splitPane = new JSplitPane();
+
+        splitPane.setLeftComponent(workoutPanel);
+        workoutPanel.add(new JLabel("Workouts"));
+        workoutPanel.add(workoutJList);
+
+        splitPane.setRightComponent(exercisePanel);
+        exercisePanel.add(new JLabel("Exercises"));
+        exercisePanel.add(exerciseJList);
+
+        textPanel.add(workoutNameLabel);
+        textPanel.add(workoutNameText);
+        textPanel.add(exerciseNameLabel);
+        textPanel.add(exerciseNameText);
+        textPanel.add(exerciseRepsLabel);
+        textPanel.add(exerciseRepsText);
+
+        buttonPanel.add(addWorkoutButton);
+        buttonPanel.add(saveButton);
+        buttonPanel.add(loadButton);
+        buttonPanel.add(addExerciseButton);
+
+        gridPanel.add(textPanel);
+        gridPanel.add(buttonPanel);
+
+        add(gridPanel, BorderLayout.SOUTH);
+        add(splitPane, BorderLayout.CENTER);
+    }
+
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
         if (actionEvent.getActionCommand().equals("add workout")) {
-            Workout w = new Workout(workoutNameText.getText());
-            collection.addWorkout(w);
-            workoutModel.addElement(workoutNameText.getText());
+            addWorkoutAction();
 
         } else if (actionEvent.getActionCommand().equals("add exercise")) {
             workoutJList.getSelectionModel().addListSelectionListener(e -> {
                 workoutJList.getSelectedValue();
             });
-            Workout w = collection.getWorkout(workoutJList.getSelectedValue());
-            int repsStringToInt = Integer.parseInt(exerciseRepsText.getText());
-            Exercise exercise = new Exercise(exerciseNameText.getText(), repsStringToInt);
-            w.addExercise(exercise);
-            exerciseModel.addElement(repsStringToInt + " " + exerciseNameText.getText());
+            addExerciseAction();
 
         } else if (actionEvent.getActionCommand().equals("save")) {
-            try {
-                jsonWriter.open();
-                jsonWriter.write(collection);
-                jsonWriter.close();
-            } catch (FileNotFoundException e) {
-                System.out.println("Unable to write to file: " + JSON_STORE);
-            }
+            saveAction();
+
         } else if (actionEvent.getActionCommand().equals("load")) {
-            try {
-                workoutModel.removeAllElements();
-                exerciseModel.removeAllElements();
-                collection = jsonReader.read();
-                for (Workout w : collection.getWorkouts()) {
-                    workoutModel.addElement(w.getWorkoutName());
-                }
-                for (Exercise e : collection.getWorkouts().get(0).getExercises()) {
-                    exerciseModel.addElement(e.getReps() + " " + e.getExerciseName());
-                }
-            } catch (IOException e) {
-                System.out.println("Unable to read from file: " + JSON_STORE);
+            loadAction();
+        }
+    }
+
+    // MODIFIES: collection
+    // EFFECTS: adds workout to the workout collection
+    public void addWorkoutAction() {
+        Workout w = new Workout(workoutNameText.getText());
+        collection.addWorkout(w);
+        workoutModel.addElement(workoutNameText.getText());
+    }
+
+    // MODIFIES: collection
+    // EFFECTS: adds exercise to selected workout
+    public void addExerciseAction() {
+        Workout w = collection.getWorkout(workoutJList.getSelectedValue());
+        int repsStringToInt = Integer.parseInt(exerciseRepsText.getText());
+        Exercise exercise = new Exercise(exerciseNameText.getText(), repsStringToInt);
+        w.addExercise(exercise);
+        exerciseModel.addElement(repsStringToInt + " " + exerciseNameText.getText());
+    }
+
+    // EFFECTS: saves GUI collection
+    public void saveAction() {
+        try {
+            jsonWriter.open();
+            jsonWriter.write(collection);
+            jsonWriter.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to write to file: " + JSON_STORE);
+        }
+    }
+
+    // EFFECTS: loads previously saved collection to GUI
+    public void loadAction() {
+        try {
+            workoutModel.removeAllElements();
+            exerciseModel.removeAllElements();
+            collection = jsonReader.read();
+            for (Workout w : collection.getWorkouts()) {
+                workoutModel.addElement(w.getWorkoutName());
             }
+            for (Exercise e : collection.getWorkouts().get(0).getExercises()) {
+                exerciseModel.addElement(e.getReps() + " " + e.getExerciseName());
+            }
+        } catch (IOException e) {
+            System.out.println("Unable to read from file: " + JSON_STORE);
         }
     }
 }
