0d11b10e9bf66d8b258f1b13dd086fa286c152ed
carinatz <s6y2b@students.cs.ubc.ca>
2020-11-19 23:59:48 -0800

    added citation

--- src/main/gui/WorkoutManagerGUI.java
+++ src/main/gui/WorkoutManagerGUI.java
@@ -14,6 +14,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 // citation: https://www.youtube.com/watch?v=iE8tZ0hn2Ws
+// citation: https://github.students.cs.ubc.ca/CPSC210/C3-LectureLabStarter.git
 // GUI class for workout manager
 public class WorkoutManagerGUI extends JFrame implements ActionListener {
 
